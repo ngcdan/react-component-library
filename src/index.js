4b59e8b88fd26b00258208cd3f9a7e7c4a4a0ba3
ngcdan <Linuss1908@gmail.com>
2021-07-28 07:10:36 +0700

    imple docs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Docs from './docs/Docs';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Docs />
   </React.StrictMode>,
   document.getElementById('root')
 );
