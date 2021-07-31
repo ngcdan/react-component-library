b34882c4470142b820c7e6b48082cadb4664eaa4
ngcdan <Linuss1908@gmail.com>
2021-07-31 12:24:55 +0700

    registration form

---
+++ src/docs/examples/RegistrationForm/ExampleRegistrationForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegistrationForm from 'r-components/RegistrationForm';
+
+export default class ExampleRegistrationForm extends React.Component {
+  onSubmit = (user) => {
+    console.log(user);
+  }
+
+  render() {
+    return <RegistrationForm onSubmit={this.onSubmit} />
+  }
+}
